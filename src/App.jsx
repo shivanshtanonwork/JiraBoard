577f93c0392c98202b164465ef7b0fc0896aa14e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-30 15:03:02 +0530

    Styling header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./app.css";
+import TaskForm from "./components/TaskForm/TaskForm";
 
 const App = () => {
   return (
     <div className="app">
-      <header className="app_header">Jira Board</header>
+      <h1 style={{ textAlign: "center", margin: "50px" }}>Jira Board</h1>
+      <TaskForm />
       <main className="app_main">
         <section className="task_column">Section 1</section>
         <section className="task_column">Section 2</section>
---
+++ src/components/TaskForm/TaskForm.jsx
@@ -0,0 +1,37 @@
+import "./TaskForm.css";
+
+const TaskForm = () => {
+  return (
+    <header className="app_header">
+      <form action="">
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enter Task Detail"
+        />
+        <div className="task_form_bottom">
+          <div>
+            <button className="tag">DEV</button>
+            <button className="tag">QA</button>
+            <button className="tag">Product Owner</button>
+          </div>
+          <div>
+            <select className="task_status">
+              <option value="Ready for Development">
+                Ready for Development
+              </option>
+              <option value="In Progress">In Progress</option>
+              <option value="Ready for test">Ready for test</option>
+              <option value="Closed">Closed</option>
+            </select>
+            <button className="task_submit" type="submit">
+              + Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskForm;
