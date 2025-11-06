ba5942b24ebb7a3d418663b6b7d812279850e593
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-11-06 17:45:55 +0530

    handling input form

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import Tag from "../Tag/Tag";
 import "./TaskForm.css";
 
 const TaskForm = () => {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "Ready for Development",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("");
+  // const handleTask = (e) => {
+  //   setTask(e.target.value);
+  // };
+  // const handleStatus = (e) => {
+  //   setStatus(e.target.value);
+  // };
+  // console.log(task, status);
   return (
     <header className="app_header">
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input
           type="text"
+          name="task"
           className="task_input"
           placeholder="Enter Task Detail"
+          onChange={handleChange}
         />
         <div className="task_form_bottom">
           <div>
@@ -17,7 +45,11 @@ const TaskForm = () => {
             <Tag tagName="Product Owner" />
           </div>
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              onChange={handleChange}
+            >
               <option value="Ready for Development">
                 Ready for Development
               </option>
